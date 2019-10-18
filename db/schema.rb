94fdf8968e884b630020bc57db9bc0526e35498c
nihaprezz <sergio13prez@gmail.com>
2019-10-18 08:46:34 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017024610) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.integer  "patient_id"
-    t.integer  "doctor_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
-    t.index ["patient_id"], name: "index_appointments_on_patient_id"
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "patients", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
